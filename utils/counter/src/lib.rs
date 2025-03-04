01fd840f3c9857d5f079eab834f41b4f03f5604d
Pranav Kumar <pcom118@gmail.com>
2025-03-05 02:29:59 +0530

    Adding multiple counters

---
+++ utils/counter/src/lib.rs
@@ -0,0 +1,23 @@
+use std::sync::atomic::{AtomicU8, AtomicU16, AtomicU32, AtomicU64, AtomicUsize, Ordering};
+
+macro_rules! define_counter {
+    ($counter_name:ident, $atomic_type:ident, $base_type:ty) => {
+        pub struct $counter_name($atomic_type);
+
+        impl $counter_name {
+            pub const fn new() -> Self {
+                $counter_name($atomic_type::new(1))
+            }
+
+            pub fn increment(&self) -> $base_type {
+                self.0.fetch_add(1, Ordering::Relaxed) as $base_type
+            }
+        }
+    };
+}
+
+define_counter!(CounterU8, AtomicU8, u8);
+define_counter!(CounterU16, AtomicU16, u16);
+define_counter!(CounterU32, AtomicU32, u32);
+define_counter!(CounterU64, AtomicU64, u64);
+define_counter!(CounterUsize, AtomicUsize, usize);
